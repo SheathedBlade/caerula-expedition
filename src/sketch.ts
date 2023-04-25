8520a10dfa1de97eeb8b1537fb70908c8ae14b37
Andrew Chuah <warlordpluto@gmail.com>
2023-04-25 15:43:51 -0400

    init final project

---
+++ src/sketch.ts
@@ -0,0 +1,14 @@
+import P5 from "p5";
+
+const sketch = (p5: P5) => {
+  p5.setup = () => {
+    const canvas = p5.createCanvas(p5.windowWidth, p5.windowHeight);
+    canvas.parent("#app");
+  };
+
+  p5.draw = () => {
+    p5.background(p5.color(220));
+  };
+};
+
+new P5(sketch);
