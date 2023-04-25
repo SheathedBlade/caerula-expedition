22654027ca774a1885feeb7b08456333afe76378
Andrew Chuah <warlordpluto@gmail.com>
2023-04-25 16:50:54 -0400

    init sketch; gotta transfer code to here

--- src/sketch.ts
+++ src/sketch.ts
@@ -1,6 +1,7 @@
 import P5 from "p5";
 
 const sketch = (p5: P5) => {
+  p5.preload = () => {};
   p5.setup = () => {
     const canvas = p5.createCanvas(p5.windowWidth, p5.windowHeight);
     canvas.parent("#app");
