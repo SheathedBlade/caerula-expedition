31775198f93c955e66949c5a3cfd520c96751e8e
Andrew Chuah <warlordpluto@gmail.com>
2023-05-02 02:18:29 -0400

    asd

--- src/sketch.ts
+++ src/sketch.ts
@@ -6,8 +6,12 @@ const sketch = (p5: p5) => {
   let score: number;
 
   p5.preload = () => {};
+
   p5.setup = () => {
-    const canvas = p5.createCanvas(p5.windowWidth, p5.windowHeight);
+    const canvas = p5.createCanvas(
+      p5.windowWidth - p5.windowWidth / 4,
+      p5.windowHeight
+    );
     canvas.parent("#app");
     p5.ellipseMode(p5.CENTER);
     score = 0;
@@ -16,10 +20,23 @@ const sketch = (p5: p5) => {
   };
 
   p5.draw = () => {
-    p5.background(p5.color(220));
+    p5.background(p5.color(18, 58, 112));
+
+    // Update game entities
     player.update(p5);
     player.display(p5);
-    p5.text("Score: " + score, p5.width / 2, 20);
+
+    // Score draw
+    p5.textSize(20);
+    p5.fill(p5.color(255));
+    p5.text("Score: " + score, 20, 30);
+  };
+
+  p5.keyPressed = () => {
+    // Space is pressed
+    if (p5.keyCode == 32) {
+    }
+    // Spawn player bullet
   };
 };
 
