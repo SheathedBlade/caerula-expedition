bf651dfc10550c1987c0ba13ccfcaedb6ecbf582
Andrew Chuah <warlordpluto@gmail.com>
2023-05-10 04:49:30 -0400

    asd

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="vite/client" />
+declare global {
+  declare function loadSound(
+    path: string | any[],
+    successCallback?: (...args: any[]) => any,
+    errorCallback?: (...args: any[]) => any,
+    whileLoading?: (...args: any[]) => any
+  ): p5.SoundFile;
+}
