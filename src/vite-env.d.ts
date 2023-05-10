eb77a68f0bebb4a2fb0d230b016df61822fae5e2
Andrew Chuah <warlordpluto@gmail.com>
2023-05-10 04:56:19 -0400

    man

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,10 +1,1 @@
 /// <reference types="vite/client" />
-declare global {
-  declare function loadSound(
-    path: string | any[],
-    successCallback?: (...args: any[]) => any,
-    errorCallback?: (...args: any[]) => any,
-    whileLoading?: (...args: any[]) => any
-  ): p5.SoundFile;
-  declare function soundFormats(formats?: string): void;
-}
